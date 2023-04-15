2486a1e867bc3312496e46de84ca9ad243e662d7
mjaumi <mjaumi2864@gmail.com>
2023-04-15 22:19:47 +0600

    base url updated

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
     reducerPath: 'taskManagerAPI',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:9000',
+        baseUrl: 'https://lws-task-manager.onrender.com',
     }),
     tagTypes: [],
     endpoints: builder => ({}),
